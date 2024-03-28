fac8ceb49f977fac252e68ab84f7b2ca44167439
Nott <nott@163.com>
2024-03-28 15:43:40 +0800

    add : post form

--- src/app/ui/common/create-form.tsx
+++ src/app/ui/common/create-form.tsx
@@ -1,16 +1,20 @@
 'use client'
-import { AlertDialog,Text,Button,Flex,Card,Separator } from "@radix-ui/themes"
+import { AlertDialog,Text,Button,Flex,Card,Separator,Strong,Heading,TextField,TextArea, Box } from "@radix-ui/themes"
 import { useFormState,useFormStatus } from "react-dom"
+import * as Label from '@radix-ui/react-label';
 import { doNewPost } from "@/app/lib/action"
+import Link from "next/link"
 export default function CreateForm() {
     const [errorMessage, dispatch] = useFormState(doNewPost, undefined);
     return(
         <AlertDialog.Root>
             {/* 发布按钮 */}
             <AlertDialog.Trigger>
+                <Link href="">
                 <Text size="2">
                     New Post
                 </Text>
+                </Link>
             </AlertDialog.Trigger>
             {/* 正文 */}
             <AlertDialog.Content style={{maxWidth:"500px"}}>
@@ -22,7 +26,18 @@ export default function CreateForm() {
                 <Separator my="3" size="4" />
                 <form action={dispatch}>
                     <Card>
-
+                        <Flex direction="column" gap="3">
+                            <Flex direction="row" gap="3" align="center">
+                                <Text>Title</Text>
+                                <input className="Input" type="text" id="firstName" placeholder="Type something" />
+                            </Flex>
+                            <Flex direction="row" gap="3" align="center">
+                                <Text>Context</Text>
+                                <Box style={{width:"300"}}>
+                                    <TextArea size="2" placeholder="Reply to comment…" />
+                                </Box>
+                            </Flex>
+                        </Flex>
                     </Card>
                 </form>
             <Flex mt="3" gap="3" justify="end">
